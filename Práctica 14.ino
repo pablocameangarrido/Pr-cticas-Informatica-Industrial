d4f883d20d4a87388e6b8303a238b4c72420fbe1
pablocameangarrido <152868590+pablocameangarrido@users.noreply.github.com>
2023-12-17 14:09:35 +0100

    Práctica 14.ino

---
+++ Práctica 14.ino
@@ -0,0 +1,33 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(10, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  if (digitalRead(10) == LOW) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10000); // Wait for 10000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(4000); // Wait for 4000 millisecond(s)
+    digitalWrite(11, HIGH);
+    digitalWrite(12, HIGH);
+    delay(10000); // Wait for 10000 millisecond(s)
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    delay(4000); // Wait for 4000 millisecond(s)
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+  }
+}
+
+void loop()
+{
+  delay(10); // Delay a little bit to improve simulation performance
+}
